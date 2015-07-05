b93c495b40590806660e7d1f5a45a09d533494eb
Jewelz Qiu <jewelz.q.915@gmail.com>
2015-07-05 23:58:44 +0800

    First commit

---
+++ app/src/main/java/com/jewelzqiu/watertools/Utils.java
@@ -0,0 +1,33 @@
+package com.jewelzqiu.watertools;
+
+/**
+ * Created by Jewelz on 2015-6-28.
+ */
+public class Utils {
+
+    public static double calculateDischarge(double velocity, double diameter) {
+        return Math.PI * diameter * diameter / 4 * velocity;
+    }
+
+    public static double calculateVelocity(double discharge, double diameter) {
+        return discharge / (Math.PI * diameter * diameter / 4);
+    }
+
+    public static double calculateDiameter(double discharge, double velocity) {
+        return Math.sqrt(discharge / velocity / Math.PI * 4);
+    }
+
+    public static double calculateHeadLoss(double length, double gradient) {
+        return length * gradient;
+    }
+
+    public static double calculateGradient(double velocity, double diameter) {
+        if (velocity >= 1.2) {
+            return 0.00107 * Math.pow(velocity, 2) * Math.pow(diameter, -1.3);
+        } else {
+            return 0.000912 * Math.pow(velocity, 2) * Math.pow(diameter, -1.3)
+                    * Math.pow((1 + 0.867 / velocity), 0.3);
+        }
+    }
+
+}
